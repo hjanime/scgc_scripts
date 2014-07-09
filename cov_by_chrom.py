dc326db12e404e3e28b6e148bae578fc9eaf3861
brwnj <brwnjm@gmail.com>
2014-07-09 13:56:06 -0400

    ensure genomecov is sorted

--- cov_by_chrom.py
+++ cov_by_chrom.py
@@ -7,7 +7,6 @@ bedtools genomecov -d -split -ibam <bam>
 """
 
 import os
-import sys
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from itertools import groupby
 from toolshed import reader
@@ -16,17 +15,19 @@ from toolshed import reader
 def main(bam, split=True):
     assert os.path.exists(bam)
     hdr = ['chrom', 'pos', 'count']
-    cmd = '| bedtools genomecov -d -split -ibam ' + bam
+    # ensure sorted by chrom, position
+    cmd = '| bedtools genomecov -d -split -ibam {bam} | sort -k1,1 -k2,2n'.format(bam=bam)
     if not split:
         cmd = cmd.replace(' -split', '')
 
     for chrom, grouped in groupby(reader(cmd, header=hdr), lambda x: x['chrom']):
-        chrom_length = 0
         total_count = 0
+        chrom_length = 0
 
         for toks in grouped:
             total_count += int(toks['count'])
-            chrom_length += 1
+            # length is final position
+            chrom_length = toks['pos']
 
         avgcov = total_count / float(chrom_length)
         print "\t".join([chrom, "%0.3f" % avgcov])
@@ -35,7 +36,7 @@ def main(bam, split=True):
 if __name__ == '__main__':
     p = ArgumentParser(description=__doc__, formatter_class=RawDescriptionHelpFormatter)
     p.add_argument('bam', help="coordinate sorted bam file")
-    p.add_argument('--no-split', action='store_false', help="do not split bam reads based on CIGAR")
+    p.add_argument('--no-split', action='store_false', help="do not split reads based on CIGAR")
 
     args = p.parse_args()
     main(args.bam, args.no_split)
