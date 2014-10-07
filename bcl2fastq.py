33ee462f3aab1216062f731c8c78e99acd03cb63
Joe Brown <brwnjm@gmail.com>
2014-10-07 14:16:14 -0400

    samplesheet checks; parallel process limits

--- bcl2fastq.py
+++ bcl2fastq.py
@@ -6,49 +6,68 @@ to be used with NextSeq data and it does not do any cleanup! Original dumped
 fastqs will remain along with all of the bcl files.
 """
 
-import csv
+from __future__ import print_function
+
+import fileinput
 import os
+import subprocess as sp
+import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, REMAINDER
-from sh import bcl2fastq, cat
+from itertools import izip_longest
 
 
-def parse_samples(sample_sheet):
-    """Parse SampleSheet.csv"""
+def process_samplesheet(samplesheet):
     samples = []
     experiment = ""
-    with open(sample_sheet, 'rU') as fh:
-        reader = csv.reader(fh)
+
+    try:
         start = False
-        for toks in reader:
-            try:
-                if toks[0] == "Experiment Name":
-                    experiment = toks[1]
-                if toks[0] == "Sample_ID":
-                    start = True
-                    continue
-            except IndexError:
-                # blank lines are expected in this file
-                pass
+        # strip whitespace and rewrite file in place
+        for toks in fileinput.input(samplesheet, mode='rU', backup='.bak', inplace=True):
+            toks = toks.rstrip("\r\n").split(',')
+            print(",".join([t.strip() for t in toks]))
+            if toks[0] == "Sample_ID":
+                start = True
+                continue
             if start:
-                samples.append(toks[0])
+                # bcl2fastq converts underscores to dashes
+                samples.append(toks[0].replace("_", "-"))
+                # location of fastq output
+                experiment = toks[8]
+    finally:
+        fileinput.close()
+
     return samples, experiment
 
 
-def main(runfolder, min_log_level, loading_threads, demultiplexing_threads,
+def main(runfolder_dir, loading_threads, demultiplexing_threads,
             processing_threads, barcode_mismatches, args):
-    samplesheet = "{runfolder}/SampleSheet.csv".format(runfolder=runfolder)
-    assert os.path.exists(samplesheet), "%s was not found." % samplesheet
-    samples, experiment = parse_samples(samplesheet)
 
+    # verify working directory
+    samplesheet = os.path.join(runfolder_dir, "SampleSheet.csv")
+    if not os.path.exists(samplesheet):
+        sys.exit("%s was not found. Exiting." % samplesheet)
+
+    # get sample names and experiment name from SampleSheet
+    samples, experiment = process_samplesheet(samplesheet)
+    if len(samples) == 0:
+        sys.exit("No samples were found in the SampleSheet. Check formatting.")
+
+    # set args for call to bcl2fastq
     if not '-w' in args or '--writing-threads' in args:
         args.append('--writing-threads')
         args.append(len(samples))
+    args.extend(['-R', runfolder_dir, '-r', loading_threads, '-d',
+        demultiplexing_threads, '-p', processing_threads,
+        '--barcode-mismatches', barcode_mismatches])
+    args.insert(0, 'bcl2fastq')
 
-    # call bcl2fastq
-    bcl2fastq(runfolder_dir=runfolder, min_log_level=min_log_level,
-        loading_threads=loading_threads, demultiplexing_threads=demultiplexing_threads,
-        processing_threads=processing_threads, barcode_mismatches=barcode_mismatches,
-        *args, _no_err=True)
+    # run bcl2fastq
+    cmd = " ".join(map(str, args))
+    print("Converting .bcl to .fastq using:")
+    print("$> ", cmd)
+    sp.check_call(cmd, shell=True)
+    print(".bcl conversion was successful")
 
     # location of fastqs from bcl2fastq
     if '-o' in args:
@@ -56,35 +75,36 @@ def main(runfolder, min_log_level, loading_threads, demultiplexing_threads,
     elif '--output-dir' in args:
         output_dir = args[args.index('--output-dir') + 1]
     else:
-        output_dir = "{runfolder}/Data/Intensities/BaseCalls".format(runfolder=runfolder)
+        output_dir = os.path.join(runfolder_dir, "Data", "Intensities", "BaseCalls")
+    output_dir = os.path.join(output_dir, experiment)
 
-    # cat files across lanes into new file per sample
-    processes = []
+    # build `cat` commands to join files across lanes
+    commands = []
     for i, sample in enumerate(samples, start=1):
-        # bcl2fastq converts underscores (from samplesheet) to dashes
-        sample = sample.replace("_", "-")
         for read in ['R1', 'R2']:
-            paths = []
-            result_file=("{output_dir}/{experiment}/{sample}_{read}.fastq.gz").format(
-                output_dir=output_dir, experiment=experiment, sample=sample, read=read)
-            for lane in xrange(1, 5):
+            cmd = ['cat']
+            result_file = "%s/%s_%s.fastq.gz" % (output_dir, sample, read)
+            for lane in [1, 2, 3, 4]:
                 # build the files paths
-                path = ("{output_dir}/{experiment}/{sample}_S{sample_num}"
-                        "_L00{lane}_{read}_001.fastq.gz").format(output_dir=output_dir,
-                            experiment=experiment, sample=sample, sample_num=i, lane=lane, read=read)
-                paths.append(path)
-            # execute file concatenation
-            processes.append(cat(paths, _out=result_file, _no_err=True, _bg=True))
-    for p in processes:
-        p.wait()
+                path = "%s/%s_S%d_L00%d_%s_001.fastq.gz" % \
+                    (output_dir, sample, i, lane, read)
+                if not os.path.exists(path):
+                    sys.exit("Can't find %s. Concatenation failed." % path)
+                cmd.append(path)
+            # using shell to ease redirection
+            commands.append(" ".join(cmd))
+
+    # execute concatenation 4 samples at a time
+    print("Joining reads across lanes")
+    groups = [(sp.Popen(cmd, shell=True) for cmd in commands)] * 4
+    for processes in izip_longest(*groups):
+        for p in filter(None, processes):
+            p.wait()
 
 
 if __name__ == '__main__':
     p = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
-    p.add_argument('runfolder', help="path to run folder")
-    p.add_argument('-l', '--min-log-level', choices=['NONE', 'FATAL', 'ERROR',
-                                                     'WARNING', 'INFO', 'DEBUG', 'TRACE'],
-                    default='INFO', help="minimum log level")
+    p.add_argument('-R', '--runfolder-dir', default=".", help="path to run folder")
     p.add_argument('-r', '--loading-threads', default=12, type=int,
                     help="threads used for loading BCL data")
     p.add_argument('-d', '--demultiplexing-threads', default=12, type=int,
