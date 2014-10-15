5f0aa6f534a5335e7adba140e413193a59cfe772
Joe Brown <brwnjm@gmail.com>
2014-10-15 10:23:08 -0400

    make output dir

--- genomecov_plot.py
+++ genomecov_plot.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 import os
 import subprocess
 import sys
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter as ADHF
 from rpy2 import robjects
 from rpy2.robjects.vectors import FloatVector
 from rpy2.robjects.packages import importr
@@ -30,7 +30,7 @@ def check_input(input_file):
     elif ext == ".bam":
         bedgraph = filename + ".bedgraph.gz"
         if not os.path.exists(bedgraph):
-            print ("Finding coverages")
+            print("Finding coverages")
             cmd = ("bedtools genomecov -bga -ibam {bam} "
                 "| bedtools sort -i - "
                 "| gzip > {bedgraph}").format(bam=input_file, bedgraph=bedgraph)
@@ -44,14 +44,14 @@ def main(input_file, output_dir, sample, reference):
     bedgraph = check_input(input_file)
     plot_file = os.path.join(output_dir, "%s_%s.png" % (sample, reference))
 
-    print ("Reading count data", file=sys.stderr)
+    print("Reading count data", file=sys.stderr)
     pos = []
     cnt = []
     for toks in reader(bedgraph, header=['chrom', 'start', 'stop', 'count']):
         pos.append(toks['start'])
         cnt.append(toks['count'])
 
-    print ("Generating plot", file=sys.stderr)
+    print("Generating plot", file=sys.stderr)
     prediction = smooth(FloatVector(pos), FloatVector(cnt), span=0.01,
         degree=0, family="gaussian", evaluation=5000)
     grdevices.png(plot_file, width=1200, height=800)
@@ -61,7 +61,7 @@ def main(input_file, output_dir, sample, reference):
 
 
 if __name__ == '__main__':
-    p = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
+    p = ArgumentParser(description=__doc__, formatter_class=ADHF)
     p.add_argument('input', help='bam or bedgraph file')
     p.add_argument('output', help='location to write the plot file')
     p.add_argument('sample', help='name of the input sample')
@@ -72,6 +72,6 @@ if __name__ == '__main__':
     if not os.path.exists(args.input):
         sys.exit("The input file does not exist. Exiting.")
     if not os.path.exists(args.output):
-        sys.exit("The output directory must exist. Exiting.")
+        os.makedirs(args.output)
 
     main(args.input, args.output, args.sample, args.reference)
